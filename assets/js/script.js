ce112139c7f6d22098e3f5716b50f810f55111f5
NaifZaghmout zaghmout <naif-zaghmout@hotmail.com>
2023-06-25 11:30:50 +0000

    add first js code

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+let quizData = [
+{
+ question : "When you drive at night, which of the following types of eyeglasses should you NOT wear?" ,
+ A: "Tinted" ,
+ B: "Bifocals" ,
+ C: "Half moon" ,
+ D: "Designe" ,
+ correct: "A" ,
+}
+
+
+]
